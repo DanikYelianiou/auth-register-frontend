12f7a62cefb5af9ae763e09e398e1e5fb873fceb
Danik <danik.elenev@celadonsoft.com>
2022-12-08 23:00:20 +0300

    fix next.config.js issues

--- next.config.js
+++ next.config.js
@@ -1,22 +1,42 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  cssModules: true,
-  cssLoaderOptions: {
-    importLoaders: 1,
-    localIdentName: "[local]___[hash:base64:5]",
-  },
-  lessLoaderOptions: {
-    javascriptEnabled: true,
-  },
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-    prependData: '@import "./styles/design/index.scss";',
-  },
-  images: {
-    domains: ['backend.presto.com', '', 'localhost'],
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 
+const securityHeaders = [
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload',
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin',
+  },
+];
+
+const nextConfig = {
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ];
+  },
 }
+
+module.exports = nextConfig
